1c998d6a0632b6e2dd7bf34ddbef5cc2f4b2acf6
JulioCesarAbreu-Sky <https://github.com/JulioCesarAbreu-Sky/meusEventos.git>
2024-01-10 12:21:57 -0300

    first commit

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,17 @@
+<h3>Fornecedor (view)</h3>
+
+{{'Texto de teste'}}
+
+<br />
+
+<?='Texto de teste'?>
+
+<br />
+
+{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
+
+@php
+
+    echo "Hello World";
+
+@endphp
